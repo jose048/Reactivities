80e387923004fe17809ebcda83e66c001bd38c80
Saurabh Joshi <saurabhjoshi0408@gmail.com>
2023-10-13 17:53:30 +1100

    End of Section 5

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Persistence;
+using Domain;
+using MediatR;
+using AutoMapper;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+
+                // activity.Title = request.Activity.Title ?? activity.Title;
+
+                _mapper.Map(request.Activity, activity);
+
+                await _context.SaveChangesAsync();
+             
+            }
+
+        }
+    }
+}
